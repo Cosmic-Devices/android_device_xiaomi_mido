51de5aa8bcd1e064ddcf56da3d4fd7cffbb209c4
Connor Kirk <connkirk@codeaurora.org>
2018-03-27 14:53:45 -0700

    mido: Add persist.radio.atfwd.start property
    Rename to persist.vendor.radio.atfwd.start.
    This property is now qualified as a platform property and is not
    whitelisted. Therefore it must be renamed to continue to be
    used as a trigger.
    
    Change-Id: Ifec0abd5ad04cd6445c856cba540d9e85ce54f0c

--- prop.mk
+++ prop.mk
@@ -157,6 +157,7 @@ persist.dbg.vt_avail_ovr=1 \
 persist.dbg.wfc_avail_ovr=1 \
 persist.radio.multisim.config=dsds \
 persist.vendor.radio.apm_sim_not_pwdn=1 \
+persist.vendor.radio.atfwd.start=true \
 persist.vendor.radio.custom_ecc=1 \
 persist.vendor.radio.hw_mbn_update=0 \
 persist.vendor.radio.rat_on=combine \
