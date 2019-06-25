a76db1d47fdfcb0b170737d8ce1d80484729b571
Murat Kozan <muratkozan350@gmail.com>
2019-06-25 12:44:16 +0300

    mido: Use persist time services

--- prop.mk
+++ prop.mk
@@ -197,7 +197,7 @@ service.qti.ims.enabled=1
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.timed.enable=true \
-persist.delta_time.enable=true
+persist.vendor.delta_time.enable=true
 
 # Tcp
 PRODUCT_PROPERTY_OVERRIDES += \
