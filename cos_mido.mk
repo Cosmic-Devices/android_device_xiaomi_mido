90dca193767c669b6adc0f96d81f3e5a426dae8b
Mr. EndLess <aryanmk.mk5@gmail.com>
2019-08-20 18:10:44 +0530

    Cosmic-ify : Fix makefile path

--- cos_mido.mk
+++ cos_mido.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/mido/device.mk)
 
 # Inherit some common Cosmic stuff.
-$(call inherit-product, vendor/cos/config/common.mk)
+$(call inherit-product, vendor/cos/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
